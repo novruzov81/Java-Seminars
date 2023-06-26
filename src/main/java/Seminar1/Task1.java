72fde199a50c0ca5c82a4a418026c7e8700d7cc7
novruzov81 <novruzov81@gmail.com>
2023-06-26 20:24:11 +0300

    Seminar1

---
+++ src/main/java/Seminar1/Task1.java
@@ -0,0 +1,34 @@
+package Seminar1;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        int n = 6; // Переменная n, для которой нужно вычислить треугольное число и факториал
+
+        int triangleNumber = calculateTriangleNumber(n);
+        int factorial = calculateFactorial(n);
+
+        System.out.println("Треугольное число для " + n + " = " + triangleNumber);
+        System.out.println("Факториал числа " + n + " = " + factorial);
+    }
+
+    public static int calculateTriangleNumber(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static int calculateFactorial(int n) {
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
